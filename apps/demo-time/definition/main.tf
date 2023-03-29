b08dd32f24a9d5017c6da99bdf6993110c5172c6
Yuwei Zhou <yuwzho@microsoft.com>
2023-03-29 12:49:45 +0800

    Update main.tf

--- apps/demo-time/definition/main.tf
+++ apps/demo-time/definition/main.tf
@@ -22,46 +22,73 @@ provider "azurerm" {
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-resource "azurerm_resource_group" "rg-asa" {
-  name     = var.resource_group_name
-  location = var.location
-}
+# resource "azurerm_resource_group" "rg-asa" {
+#   name     = var.resource_group_name
+#   location = var.location
+# }
 
-resource "azurerm_spring_cloud_service" "demo-time-asa" {
-  name                     = "demo-time-asa"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  sku_name                 = "E0"
-  service_registry_enabled = true
-  build_agent_pool_size    = "S1"
-  timeouts {
-  }
-}
+# resource "azurerm_spring_cloud_service" "demo-time-asa" {
+#   name                     = "demo-time-asa"
+#   resource_group_name      = var.resource_group_name
+#   location                 = var.location
+#   sku_name                 = "E0"
+#   service_registry_enabled = true
+#   build_agent_pool_size    = "S1"
+#   timeouts {
+#   }
+# }
 
-resource "azurerm_spring_cloud_configuration_service" "configservice" {
-  name                    = "default"
-  spring_cloud_service_id = azurerm_spring_cloud_service.demo-time-asa.id
-}
+# resource "azurerm_spring_cloud_configuration_service" "configservice" {
+#   name                    = "default"
+#   spring_cloud_service_id = azurerm_spring_cloud_service.demo-time-asa.id
+# }
 
-resource "azurerm_spring_cloud_gateway" "scgateway" {
-  name                    = "default"
-  spring_cloud_service_id = azurerm_spring_cloud_service.demo-time-asa.id
-  instance_count          = 2
-}
+# resource "azurerm_spring_cloud_gateway" "scgateway" {
+#   name                    = "default"
+#   spring_cloud_service_id = azurerm_spring_cloud_service.demo-time-asa.id
+#   instance_count          = 2
+# }
 
-resource "azurerm_spring_cloud_api_portal" "apiportal" {
-  name                          = "default"
-  spring_cloud_service_id       = azurerm_spring_cloud_service.demo-time-asa.id
-  gateway_ids                   = [azurerm_spring_cloud_gateway.scgateway.id]
-  https_only_enabled            = false
-  public_network_access_enabled = false
-  instance_count                = 1
-  timeouts {
-  }
+# resource "azurerm_spring_cloud_api_portal" "apiportal" {
+#   name                          = "default"
+#   spring_cloud_service_id       = azurerm_spring_cloud_service.demo-time-asa.id
+#   gateway_ids                   = [azurerm_spring_cloud_gateway.scgateway.id]
+#   https_only_enabled            = false
+#   public_network_access_enabled = false
+#   instance_count                = 1
+#   timeouts {
+#   }
+# }
+
+data "azurerm_spring_cloud_service" "service" {
+  name                = "${var.service_name}"
+  resource_group_name = "${var.resource_group_name}"
 }
 
 resource "azurerm_spring_cloud_app" "demo-time-app" {
   name                = "demo-time"
-  resource_group_name = var.resource_group_name
-  service_name        = azurerm_spring_cloud_service.demo-time-asa.name
+  resource_group_name = "${var.resource_group_name}"
+  service_name        = "${var.service_name}"
+}
+
+resource "azurerm_spring_cloud_build_deployment" "sample_1_deployment" {
+  name                = "blue"
+  spring_cloud_app_id = azurerm_spring_cloud_app.demo-time-app.id
+  build_result_id     = "<default>"
+  instance_count      = 2
+  quota {
+    cpu    = "2"
+    memory = "4Gi"
+  }
+}
+
+resource "azurerm_spring_cloud_build_deployment" "sample_1_deployment" {
+  name                = "green"
+  spring_cloud_app_id = azurerm_spring_cloud_app.demo-time-app.id
+  build_result_id     = "<default>"
+  instance_count      = 2
+  quota {
+    cpu    = "2"
+    memory = "4Gi"
+  }
 }
