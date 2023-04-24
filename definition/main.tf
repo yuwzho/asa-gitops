ce68ddf84f57f713c69598a74fc75544696290fc
Yuwei Zhou <yuwzho@microsoft.com>
2023-04-24 13:19:36 +0800

    update workflow

--- definition/main.tf
+++ definition/main.tf
@@ -9,7 +9,7 @@ terraform {
   # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "ASA-E-GitOps-State"
-    storage_account_name = "asaegitopstfstate"
+    storage_account_name = "asaegitopstfstatetest"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
